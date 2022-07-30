2bdfda4f93dab409de07ef3c98d565a4e935a62b
Gus <gus.cuddy@gmail.com>
2022-07-30 01:29:58 -0400

    fix: removed prose desaturation effect

--- src/lib/components/ProseWrapper.svelte
+++ src/lib/components/ProseWrapper.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	class="prose prose-neutral prose-amber mx-auto block break-words transition prose-a:transition prose-figure:drop-shadow-sm prose-figcaption:mx-auto  prose-figcaption:max-w-sm prose-figcaption:text-center prose-img:mx-auto  prose-img:rounded dark:prose-invert dark:prose-a:saturate-50 dark:hover:prose-a:saturate-100 {breakpoints
+	class="prose prose-neutral prose-amber mx-auto block break-words transition prose-a:transition prose-figure:drop-shadow-sm prose-figcaption:mx-auto  prose-figcaption:max-w-sm prose-figcaption:text-center prose-img:mx-auto  prose-img:rounded dark:prose-invert  {breakpoints
 		? 'md:prose-lg'
 		: ''} {className}"
 >
