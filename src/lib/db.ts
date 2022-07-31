66791fcf68eff94d2de9c08e7c608c79b534537e
Gus <gus.cuddy@gmail.com>
2022-07-31 13:32:16 -0400

    ops: prisma logs queries in dev mode now

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,7 +13,9 @@ if (process.env.NODE_ENV === 'production') {
 	db = new PrismaClient();
 } else {
 	if (!global.__db) {
-		global.__db = new PrismaClient();
+		global.__db = new PrismaClient({
+			log: ['query']
+		});
 	}
 	db = global.__db;
 }
