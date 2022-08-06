6325879e229dbd6db294626fe931e2362be214d5
Gus <gus.cuddy@gmail.com>
2022-08-06 17:04:29 -0400

    genericlist component changes

--- src/lib/components/GenericList.svelte
+++ src/lib/components/GenericList.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	type T = $$Generic;
 	export let items: T[];
+	export let idResolver: (item: T) => string = (item) => (item as any).id;
 </script>
 
-<ul />
+<ul>
+	{#each items as item (idResolver(item))}
+		<li>
+			<span>{item.title}</span>
+		</li>
+	{/each}
+</ul>
