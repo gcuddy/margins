d6e4c2912de1131ac88504cd777e44fbf9cb88bf
Gus <gus.cuddy@gmail.com>
2022-08-07 11:11:55 -0400

    experimenting with tailwind keyframes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,12 +22,64 @@ const config = {
 				sans: [...defaultTheme.fontFamily.sans]
 			},
 			colors: {
-				light: colors.neutral[50],
-				dark: colors.neutral[800],
-				primary: colors.amber,
-				'gray-350': 'rgb(188,188,188)',
-				'gray-750': '#333',
-				'gray-850': 'rgb(30,30,30)'
+				light: colors.stone[50],
+				dark: colors.stone[800],
+				primary: colors.amber
+				// 'gray-350': 'rgb(188,188,188)',
+				// 'gray-700': '#333',
+				// 'gray-850': 'rgb(30,30,30)'
+			},
+			keyframes: {
+				'fade-in-bg': {
+					'0%': {
+						'--tw-bg-opacity': '0'
+					},
+					'10%': {
+						'--tw-bg-opacity': '0.1'
+					},
+					'20%': {
+						'--tw-bg-opacity': '0.2'
+					},
+					'30%': {
+						'--tw-bg-opacity': '0.3'
+					},
+					'40%': {
+						'--tw-bg-opacity': '0.4'
+					},
+					'50%': {
+						'--tw-bg-opacity': '0.5'
+					},
+					'60%': {
+						'--tw-bg-opacity': '0.6'
+					},
+					'70%': {
+						'--tw-bg-opacity': '0.7'
+					},
+					'80%': {
+						'--tw-bg-opacity': '0.8'
+					},
+					'90%': {
+						'--tw-bg-opacity': '0.9'
+					},
+					'100%': {
+						'--tw-bg-opacity': '1'
+					}
+				},
+				'saturate-pulse': {
+					'0%': {
+						filter: 'saturate(1)'
+					},
+					'50%': {
+						filter: 'saturate(1.25)'
+					},
+					'100%': {
+						filter: 'saturate(1)'
+					}
+				}
+			},
+			animation: {
+				'fade-in-bg': 'fade-in-bg 0.1s ease-in-out',
+				'saturate-pulse': 'saturate-pulse 0.5s ease-in-out'
 			},
 			transitionDuration: {
 				quick: '0.15s'
