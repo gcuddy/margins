d10239e938689328ef932140605296ba4602ae10
Gus <gus.cuddy@gmail.com>
2022-09-02 10:52:40 -0400

    fix: providing userdata and redirects

--- src/routes/(marketing)/+page.ts
+++ src/routes/(marketing)/+page.ts
@@ -1,7 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-// export const load: PageLoad = async ({ parent }) => {
-// 	const { lucia } = await parent();
-// 	if (lucia) throw redirect(302, '/inbox');
-// };
+export const load: PageLoad = async ({ parent }) => {
+	// this might be causing the redirect problem?
+	const { lucia } = await parent();
+	if (lucia) throw redirect(302, '/inbox');
+};
--- src/routes/api/fetch_user_data/+server.ts
+++ src/routes/api/fetch_user_data/+server.ts
@@ -7,23 +7,24 @@ export const GET: RequestHandler = async ({ request, url: Url }) => {
 	try {
 		const user = await auth.validateRequest(request);
 		console.log(`here's what we got for fetch_user_data`, { user });
-		// const userData = await db.user.findFirst({
-		// 	where: {
-		// 		id: user.user['user_id'],
-		// 	},
-		// 	select: {
-		// 		username: true,
-		// 		feeds: true,
-		// 		favorites: true,
-		// 		articles: {
-		// 			select: ArticleListSelect,
-		// 		},
-		// 	},
-		// });
-		// console.log(`Here's our userData`, { userData });
-		// if (!userData) {
-		// 	throw error(400, 'User not found');
-		// }
+		const userData = await db.user.findFirst({
+			where: {
+				id: user.user['user_id'],
+			},
+			select: {
+				username: true,
+				feeds: true,
+				favorites: true,
+				articles: {
+					select: ArticleListSelect,
+				},
+			},
+		});
+		console.log(`Here's our userData`, { userData });
+		if (!userData) {
+			throw error(400, 'User not found');
+		}
+		return json(userData);
 		return json({
 			username: 'test',
 			feeds: [],
