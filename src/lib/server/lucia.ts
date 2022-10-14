5482b2fd9f93015802d1c48a6b1260eb77c8b7f8
Gus <gus.cuddy@gmail.com>
2022-10-14 16:19:01 -0400

    updated signup

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,9 @@
+import lucia from 'lucia-sveltekit';
+import prisma from '@lucia-sveltekit/adapter-prisma';
+import { dev } from '$app/environment';
+import { db } from '$lib/db';
+
+export const auth = lucia({
+	adapter: prisma(db),
+	env: dev ? 'DEV' : 'PROD',
+});
--- src/routes/(login)/signup/+page.server.ts
+++ src/routes/(login)/signup/+page.server.ts
@@ -1,51 +1,41 @@
-import type { Action } from './$types';
-import { auth } from '$lib/lucia';
+import { auth } from '$lib/server/lucia';
+import { invalid, redirect, type Actions } from '@sveltejs/kit';
+import { setCookie } from 'lucia-sveltekit';
 
-export const POST: Action = async ({ request, setHeaders }) => {
-	const form = await request.formData();
-	const username = form.get('username');
-	const password = form.get('password');
-	if (!username || !password || typeof username !== 'string' || typeof password !== 'string') {
-		return {
-			errors: {
+export const actions: Actions = {
+	default: async ({ request, cookies }) => {
+		const form = await request.formData();
+		const email = form.get('email');
+		const password = form.get('password');
+		if (!email || !password || typeof email !== 'string' || typeof password !== 'string') {
+			return invalid(400, {
 				message: 'Invalid input',
-				username: '',
-			},
-		};
-	}
-	try {
-		const createUser = await auth.createUser('username', username, {
-			password,
-			user_data: {
-				username,
-			},
-		});
-		console.log({ createUser });
-		setHeaders({
-			'set-cookie': createUser.cookies,
-		});
-		return {
-			location: '/inbox',
-		};
-	} catch (e) {
-		const error = e as Error;
-		if (
-			error.message === 'AUTH_DUPLICATE_IDENTIFIER_TOKEN' ||
-			error.message === 'AUTH_DUPLICATE_USER_DATA'
-		) {
-			return {
-				errors: {
-					username: 'Username already taken',
-					message: '',
-				},
-			};
+			});
 		}
-		return {
-			status: 500,
-			errors: {
-				message: 'Unknown error',
-				username: '',
-			},
-		};
-	}
+		try {
+			const user = await auth.createUser('username', email, {
+				password,
+				userData: {
+					email,
+				},
+			});
+			const { session, tokens } = await auth.createSession(user.userId);
+			setCookie(cookies, ...tokens.cookies);
+		} catch (e) {
+			const error = e as Error;
+			if (
+				error.message === 'AUTH_DUPLICATE_IDENTIFIER_TOKEN' ||
+				error.message === 'AUTH_DUPLICATE_USER_DATA'
+			) {
+				return invalid(400, {
+					message: 'Username unavailable',
+				});
+			}
+			console.error(error);
+			return invalid(500, {
+				message: 'Unknown error occurred',
+			});
+		}
+		throw redirect(302, '/login');
+	},
 };
