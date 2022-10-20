53da5b1f621a72c5c54393ce0921e146e1df9f4b
Gus <gus.cuddy@gmail.com>
2022-10-20 01:29:36 -0400

    feat: opml upload

--- src/routes/(app)/Sidebar.svelte
+++ src/routes/(app)/Sidebar.svelte
@@ -143,7 +143,6 @@
 	} else {
 		width = _width;
 	}
-	$: console.log({ $page });
 </script>
 
 {#if sidebarToggle}
---
+++ src/routes/(app)/rss/opml/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { enhance } from '$app/forms';
+	import Button from '$lib/components/Button.svelte';
+	import DotMenu from '$lib/components/DotMenu.svelte';
+	import { notifications } from '$lib/stores/notifications';
+	/** @type {import('./$types').ActionData} */
+	export let form;
+	$: console.log({ form });
+</script>
+
+<div class="container mx-auto px-4">
+	<h1>OPML</h1>
+	{#if !form || form?.message}
+		{#if form?.message}
+			<p>{form.message}</p>
+		{/if}
+		<form use:enhance action="?/submitOpml">
+			<label class="block"
+				><span class="sr-only">Upload your OPML File.</span>
+				<input
+					type="file"
+					name="opml"
+					id="opml-upload"
+					class="block w-full text-sm text-gray-500 file:mr-4 file:rounded-full file:border-0 file:bg-primary-50 file:py-2 file:px-4 file:text-sm file:font-semibold file:text-primary-700 file:hover:bg-primary-100"
+					accept=".opml"
+				/>
+			</label>
+			<button type="submit">Submit</button>
+		</form>
+	{:else}
+		<form
+			action="?/submitUrls"
+			use:enhance={() => {
+				notifications.notify({
+					message:
+						'URLs are being processed in the background. Please refresh in a little bit to see the results.',
+					title: 'Processing urls',
+				});
+			}}
+		>
+			<div class="flex flex-col space-y-4 border">
+				<div class="flex items-center space-x-4 px-2">
+					<div class="grid grow grid-cols-2 place-items-center">
+						<span>Name</span>
+						<span>URL</span>
+					</div>
+				</div>
+				<ul class="max-h-52 overflow-y-auto">
+					{#each form.opml.body.outline.filter((o) => o.type === 'rss' && o.xmlUrl) as item}
+						<li class="flex items-center space-x-4 px-2">
+							<input type="checkbox" name="url" value={item.xmlUrl} class="shrink-0" />
+							<div class="grid grow grid-cols-2">
+								<span class="truncate">{item.text}</span>
+								<span class="truncate">{item.xmlUrl}</span>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<Button type="submit">Submit</Button>
+		</form>
+	{/if}
+</div>
