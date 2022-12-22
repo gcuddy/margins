236838c166d4eecb42a37c0bc5ce575dc0af7d46
Gus <gus.cuddy@gmail.com>
2022-12-22 00:20:32 -0500

    before update

--- src/routes/(app)/u:[username]/add/+page.server.ts
+++ src/routes/(app)/u:[username]/add/+page.server.ts
@@ -2,10 +2,9 @@
 
 import { DocumentType } from '@prisma/client';
 import { error, fail } from '@sveltejs/kit';
-import dayjs from 'dayjs';
 
 import { db } from '$lib/db';
-import parse from '$lib/parse';
+import { connectOrCreateTaggings } from '$lib/tag.server';
 
 import type { Actions, PageServerLoad } from './$types';
 
@@ -23,12 +22,15 @@ export const load: PageServerLoad = async ({ url }) => {
 };
 
 export const actions: Actions = {
-	default: async ({ request, locals }) => {
+	default: async ({ request, locals, fetch }) => {
 		console.log({ locals });
 		// todo: handle image requests and such
 		try {
-			const { userId } = await locals.validate();
-			console.log({ userId });
+			const { session, user } = await locals.validateUser();
+			if (!session) {
+				throw error(401);
+			}
+			const { userId } = session;
 			const form = await request.formData();
 			const url = <string>form.get('url') || <string>form.get('text');
 			if (!url) {
@@ -40,73 +42,142 @@ export const actions: Actions = {
 			const note = <string>form.get('note');
 			const title = <string>form.get('title');
 			const html = <string>form.get('html');
-			const tags = <string[]>form.getAll('tags[]');
+			const tags = <string[]>form.getAll('tags');
 			const _private = <string>form.get('private');
-			console.log({ _private });
 			const location = <string>form.get('location');
 			const readLater = <string>form.get('readLater');
-			console.log({ tags, note });
 			const contextUrl = <string | undefined>form.get('context-url');
-			const article = await parse(url, html || undefined);
-			console.log({ article });
+			const article = await fetch(`/api/parse?url=${encodeURIComponent(url)}`).then((res) =>
+				res.json()
+			);
+			console.log(`result of api/parse.json`, { article });
+			// const article = await parse(url, html || undefined);
 			let context;
-			await db.entry.upsert({
-				data: {
-					...article,
-					type: DocumentType.article,
-					uri: url,
-				},
-				where: {},
-			});
-			const newArticle = await db.article.create({
-				data: {
-					...article,
-					url,
-					title: title || article.title || '',
-					author: article.author || '',
-					image: article.image || '',
-					date: dayjs(article.date).isValid() ? dayjs(article.date).format() : dayjs().format(),
-					readProgress: 0,
-					context,
-					type: 0,
-					userId,
-					private: _private === 'on',
-					location,
-					readLater: readLater === 'on',
-					tags: tags.length
-						? {
-								connectOrCreate: tags.map((t) => {
-									return {
-										where: {
-											name_userId: {
-												name: t,
-												userId,
-											},
-										},
-										create: {
-											name: t,
-											userId,
-										},
-									};
-								}),
-						  }
-						: undefined,
-					annotations: !note
-						? undefined
-						: {
-								create: {
-									target: JSON.stringify({
-										source: url,
-									}),
-									motivation: 'describing',
-									body: note,
+			// TODO: normalize url
+			const [entry, bookmark] = await db.$transaction([
+				db.entry.upsert({
+					where: {
+						uri: url,
+					},
+					create: {
+						...article,
+						type: DocumentType.article,
+						uri: url,
+					},
+					update: {
+						...article,
+						uri: url,
+					},
+				}),
+				db.bookmark.upsert({
+					where: {
+						uri: url,
+					},
+					create: {
+						data: article,
+						interaction: {
+							connectOrCreate: {
+								where: {
+									userId_uri: {
+										uri: url,
+										userId,
+									},
 								},
-						  },
-				},
-			});
-			if (newArticle) {
+								create: {
+									userId,
+									uri: url,
+								},
+							},
+						},
+						tags: {
+							connectOrCreate: connectOrCreateTaggings({ tags, userId }),
+						},
+						annotations: note
+							? {
+									create: {
+										type: 'note',
+										userId,
+										body: note,
+									},
+							  }
+							: undefined,
+						entry: {
+							connect: {
+								uri: url,
+							},
+						},
+						user: {
+							connect: {
+								id: userId,
+							},
+						},
+						state: {
+							connect: {
+								id: user.default_state_id,
+							},
+						},
+					},
+					update: {
+						data: article,
+					},
+				}),
+			]);
+			console.log({ entry, bookmark });
+			// const bookmark = await db.annotation.upsert({
+			//     where: {
+
+			//     }
+			// })
+			// const newArticle = await db.article.create({
+			// 	data: {
+			// 		...article,
+			// 		url,
+			// 		title: title || article.title || '',
+			// 		author: article.author || '',
+			// 		image: article.image || '',
+			// 		date: dayjs(article.date).isValid() ? dayjs(article.date).format() : dayjs().format(),
+			// 		readProgress: 0,
+			// 		context,
+			// 		type: 0,
+			// 		userId,
+			// 		private: _private === 'on',
+			// 		location,
+			// 		readLater: readLater === 'on',
+			// 		tags: tags.length
+			// 			? {
+			// 					connectOrCreate: tags.map((t) => {
+			// 						return {
+			// 							where: {
+			// 								name_userId: {
+			// 									name: t,
+			// 									userId,
+			// 								},
+			// 							},
+			// 							create: {
+			// 								name: t,
+			// 								userId,
+			// 							},
+			// 						};
+			// 					}),
+			// 			  }
+			// 			: undefined,
+			// 		annotations: !note
+			// 			? undefined
+			// 			: {
+			// 					create: {
+			// 						target: JSON.stringify({
+			// 							source: url,
+			// 						}),
+			// 						motivation: 'describing',
+			// 						body: note,
+			// 					},
+			// 			  },
+			// 	},
+			// });
+			if (entry) {
 				return {
-					location: `/article/${newArticle.id}`,
+					// location: `/u:${}/article/${newArticle.id}`,
+					entry,
 				};
 			}
 		} catch (e) {
