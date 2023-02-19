61fd3dc94febed937b69d81636512958a5378e9b
Gus <gus.cuddy@gmail.com>
2023-02-18 20:40:57 -0500

    does this work

--- src/lib/features/entries/bookmarks/image.ts
+++ src/lib/features/entries/bookmarks/image.ts
@@ -1,8 +1,5 @@
 import chromium from '@sparticuz/chromium';
 import puppeteer from 'puppeteer-core';
-import { addExtra } from 'puppeteer-extra';
-import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 
 export async function getScreenshot(url: string) {
@@ -11,25 +8,45 @@ export async function getScreenshot(url: string) {
     console.time("scrape")
 
     try {
-        const options = {
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            headless: chromium.headless,
-            executablePath: await chromium.executablePath(),
-            ignoreHTTPSErrors: true
-        };
-        const extra = addExtra(puppeteer);
-        const stealth = StealthPlugin();
-        stealth.enabledEvasions.delete('user-agent-override');
-        extra.use(stealth);
-        const adblocker = AdblockerPlugin();
-        extra.use(adblocker);
-
-        browser = await extra.launch(options);
+        const options = process.env.AWS_REGION
+            ? {
+                args: chromium.args,
+                executablePath: await chromium.executablePath(),
+                headless: chromium.headless,
+            }
+            : {
+                args: [],
+                executablePath:
+                    process.platform === 'win32'
+                        ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+                        : process.platform === 'linux'
+                            ? '/usr/bin/google-chrome'
+                            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+            };
+        browser = await puppeteer.launch(options);
         const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
-        await page.goto(url, { waitUntil: 'networkidle0'});
-      result = await page.screenshot({type: 'png'})
+        await page.setViewport({ width: 2000, height: 1000 });
+        await page.goto(url, { waitUntil: 'networkidle0' });
+        result = await page.screenshot({ type: 'png' });
+        //     const options = {
+        //         args: chromium.args,
+        //         defaultViewport: chromium.defaultViewport,
+        //         headless: chromium.headless,
+        //         executablePath: await chromium.executablePath(),
+        //         ignoreHTTPSErrors: true
+        //     };
+        //     const extra = addExtra(puppeteer);
+        //     const stealth = StealthPlugin();
+        //     stealth.enabledEvasions.delete('user-agent-override');
+        //     extra.use(stealth);
+        //     const adblocker = AdblockerPlugin();
+        //     extra.use(adblocker);
+
+        //     browser = await extra.launch(options);
+        //     const page = await browser.newPage();
+        //     await page.setViewport({ width: 1920, height: 1080 });
+        //     await page.goto(url, { waitUntil: 'networkidle0'});
+        //   result = await page.screenshot({type: 'png'})
     } catch (error) {
         console.error(error);
         return result;
