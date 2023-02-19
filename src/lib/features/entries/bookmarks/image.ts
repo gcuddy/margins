5000f47bc37b763572cb24f2a02d5db23b8b0e6d
Gus <gus.cuddy@gmail.com>
2023-02-18 20:33:43 -0500

    new screenshot hopefully minified

--- src/lib/features/entries/bookmarks/image.ts
+++ src/lib/features/entries/bookmarks/image.ts
@@ -1,110 +1,43 @@
-import chrome from "chrome-aws-lambda";
-import pkg from 'puppeteer-core';
-const { launch } = pkg;
-// import typ{ launch, Page } from "puppeteer-core";
-type Page = typeof pkg["Page"]
+import chromium from '@sparticuz/chromium';
+import puppeteer from 'puppeteer';
+import { addExtra } from 'puppeteer-extra';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-interface ScreenshotOptions {
-    url: string;
-    width: number;
-    height: number;
-    css: string;
-    js: string;
-    deviceScaleFactor: number;
-    type: "jpeg" | "png";
-    waitUntil: "load" | "domcontentloaded" | "networkidle0" | "networkidle2";
-}
 
-let _page: Page | null;
+export async function getScreenshot(url: string) {
+    let result: null | Buffer = null;
+    let browser = null;
+    console.time("scrape")
 
-async function getPage() {
-    let page = _page;
-    if (!page) {
-        const chromePathOnLambda = await chrome.executablePath;
-        console.log({ chromePathOnLambda });
-        const fonts = [
-            "https://cdn.jsdelivr.net/gh/googlei18n/noto-emoji@948b1a7f1ed4ec7e27930ad8e027a740db3fe25e/fonts/NotoColorEmoji.ttf",
-            "https://cdn.jsdelivr.net/gh/googlefonts/Arimo@dcb3e77c8800e3a35974ce45e23e1a983e1682d4/fonts/ttf/Arimo-Regular.ttf",
-            "https://cdn.jsdelivr.net/gh/googlefonts/Arimo@dcb3e77c8800e3a35974ce45e23e1a983e1682d4/fonts/ttf/Arimo-Bold.ttf",
-            "https://cdn.jsdelivr.net/gh/googlefonts/Arimo@dcb3e77c8800e3a35974ce45e23e1a983e1682d4/fonts/ttf/Arimo-Italic.ttf",
-            "https://cdn.jsdelivr.net/gh/googlefonts/Arimo@dcb3e77c8800e3a35974ce45e23e1a983e1682d4/fonts/ttf/Arimo-BoldItalic.ttf",
-            "https://cdn.jsdelivr.net/gh/googlefonts/noto-fonts@8194fd72cbc46bb88e8246b68e42b96cbef0c700/hinted/ttf/NotoSans/NotoSans-Regular.ttf",
-            "https://cdn.jsdelivr.net/gh/googlefonts/noto-cjk@165c01b46ea533872e002e0785ff17e44f6d97d8/Sans/OTC/NotoSansCJK-Regular.ttc",
-            "https://cdn.jsdelivr.net/gh/googlefonts/noto-fonts@8194fd72cbc46bb88e8246b68e42b96cbef0c700/hinted/ttf/NotoSansThai/NotoSansThai-Regular.ttf",
-        ];
-        await Promise.all(
-            fonts.map(async (f) =>
-                chrome.font(f)?.catch((e) => {
-                    console.error("Unable to fetch font %s", f, e);
-                })
-            )
-        );
-        // Disabled because it is not compatible with the latest version…
-        // await patchFontConfig()
-        const browser = await launch({
-            args: [...chrome.args, "--font-render-hinting=none"],
-            executablePath: chromePathOnLambda || "/opt/homebrew/bin/chromium",
-            headless: true,
-        });
-        console.log({ browser });
-        page = await browser.newPage();
-        _page = page;
-    }
-    return page;
-}
+    try {
+        const options = {
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless,
+            executablePath: await chromium.executablePath(),
+            ignoreHTTPSErrors: true
+        };
+        const extra = addExtra(puppeteer);
+        const stealth = StealthPlugin();
+        stealth.enabledEvasions.delete('user-agent-override');
+        extra.use(stealth);
+        const adblocker = AdblockerPlugin();
+        extra.use(adblocker);
 
-export async function renderImage({
-    url,
-    type,
-    height,
-    width,
-    css,
-    js,
-    deviceScaleFactor,
-    waitUntil,
-}: ScreenshotOptions) {
-    const page = await getPage();
-    await page.setViewport({ width, height, deviceScaleFactor });
-    await page.goto(url, { waitUntil, timeout: 6400 }).catch((e) => console.error(e));
-    if (js) {
-        await page
-            .evaluate(async (code) => {
-                try {
-                    await eval(code);
-                } catch (e) {
-                    const errorBox = document.createElement("personal-puppeteer-error-box");
-                    errorBox.setAttribute(
-                        "style",
-                        `position: fixed; bottom: 0; right: 0; background: red; color: white; z-index: 999999999; padding: 10px;`
-                    );
-                    errorBox.textContent = String(e);
-                    document.body.appendChild(errorBox);
-                }
-            }, js)
-            .catch(console.error);
-    }
-    // See: https://github.com/puppeteer/puppeteer/issues/511
-    await page.evaluate(async (css) => {
-        const style = document.createElement("style");
-        style.textContent = `
-        *,
-        *::after,
-        *::before {
-          transition-delay: 0s !important;
-          transition-duration: 0s !important;
-          animation-delay: -0.0001s !important;
-          animation-duration: 0s !important;
-          animation-play-state: paused !important;
-          caret-color: transparent !important;
-          color-adjust: exact !important;
+        browser = await extra.launch(options);
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.goto(url, { waitUntil: 'networkidle0'});
+      result = await page.screenshot({type: 'png'})
+    } catch (error) {
+        console.error(error);
+        return result;
+    } finally {
+        if (browser !== null) {
+            await browser.close();
         }
-        ${css}
-      `;
-        document.head.appendChild(style);
-        document.documentElement.dataset.screenshotMode = "1";
-        await new Promise(requestAnimationFrame);
-        await new Promise(requestAnimationFrame);
-    }, css || "");
-    const file = await page.screenshot({ type });
-    return file;
+    }
+    console.timeEnd('scrape');
+    return result;
 }
--- src/lib/trpc/routes/public.ts
+++ src/lib/trpc/routes/public.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 import { uploadFile } from "$lib/backend/s3.server";
-import { renderImage } from "$lib/features/entries/bookmarks/image"
+import { getScreenshot } from "$lib/features/entries/bookmarks/image";
 import { normalizeUrl } from "$lib/feeds/utils";
 import parse from "$lib/parse";
 import { publicProcedure, router } from "$lib/trpc/t";
@@ -49,17 +49,20 @@ export const publicRouter = router({
 			// const res = await fetch(`/api/screenshot/${encodeURIComponent(input)}`)
 
 			// TODO: CACHE THIS VERY AGGRESSIVELY (1 day)
-			const image = await renderImage({
-				css,
-				url,
-				type,
-				waitUntil,
-				height,
-				width,
-				deviceScaleFactor,
-				js
-			})
-
+			// const image = await renderImage({
+			// 	css,
+			// 	url,
+			// 	type,
+			// 	waitUntil,
+			// 	height,
+			// 	width,
+			// 	deviceScaleFactor,
+			// 	js
+			// })
+           const image = await getScreenshot(url)
+            if (!image) {
+                throw new Error("No image");
+            }
 			// upload image to s3
 			// if (!image) {
 			// 	throw new Error("No image");
@@ -68,6 +71,7 @@ export const publicRouter = router({
 			const Key = `screenshots/${url.replace(/[^a-zA-Z0-9]/g, "_")}.${type}`
 			const data = await uploadFile({
 				Key,
+                // @ts-expect-error
 				Body: image
 			});
 			return Key;
