b86d5d6fb6854aa3e7e9bb6913560232413a8599
Gus <gus.cuddy@gmail.com>
2023-03-03 10:27:15 -0500

    a couple annoying schema changes to make planetscale happy

--- tasks/db/scratch.js
+++ tasks/db/scratch.js
@@ -2,17 +2,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    const entry = await prisma.entry.deleteMany({
-        where:{
-           feedId: 32,
-           published: {
-                // less than one month ago
-                lt: new Date(new Date().setDate(new Date().getDate() - 30))
-           }
-        },
-    })
-    console.log(entry.length)
-	// })
+    const users = await prisma.user.findMany()
+    console.log({users})
 }
 
 main();
