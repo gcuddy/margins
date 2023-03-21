e66fecebd996058ff76925a0028c7f0460f806ee
Gus <gus.cuddy@gmail.com>
2023-03-21 14:33:28 -0400

    -spotify

--- src/lib/features/services/spotify.ts
+++ src/lib/features/services/spotify.ts
@@ -3,11 +3,13 @@ import Spotify from "spotify-web-api-node";
 import { SPOTIFY_CLIENT_SECRET } from "$env/static/private";
 import { PUBLIC_SPOTIFY_CLIENT_ID } from "$env/static/public";
 
-export const spotify = new Spotify({
-	clientId: PUBLIC_SPOTIFY_CLIENT_ID,
-	clientSecret: SPOTIFY_CLIENT_SECRET,
-});
+// export const spotify = new Spotify({
+// 	clientId: PUBLIC_SPOTIFY_CLIENT_ID,
+// 	clientSecret: SPOTIFY_CLIENT_SECRET,
+// });
 
-spotify.clientCredentialsGrant().then((data) => {
-	spotify.setAccessToken(data.body["access_token"]);
-});
+// spotify.clientCredentialsGrant().then((data) => {
+// 	spotify.setAccessToken(data.body["access_token"]);
+// });
+
+export const spotify ={}
