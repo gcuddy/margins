423b10197adf3a1cc5d5890831b2da8b0079631a
Gus <gus.cuddy@gmail.com>
2023-03-23 16:36:13 -0400

    rss node

--- src/routes/(app)/u:[username]/rss/+layout.ts
+++ src/routes/(app)/u:[username]/rss/+layout.ts
@@ -1,3 +1,8 @@
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+    runtime: "nodejs18.x",
+}
 export const load: LayoutLoad = async () => {
-	console.log('running layout load for /rss');
+    console.log('running layout load for /rss');
 };
