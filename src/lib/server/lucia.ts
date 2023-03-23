55e66db5554c610f2b2f6145d5dda64d7e5ab538
Gus <gus.cuddy@gmail.com>
2023-03-23 01:25:12 -0400

    switch adapter off redis

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -8,23 +8,24 @@ import kysely from "$lib/auth/kysley-pscale-adapter"
 import { Kysely } from "kysely";
 import { db } from "$lib/db";
 export const auth = lucia({
-     // TODO: type error here?
-	adapter: {
-        user: kysely(db),
-        session: redisSessionAdapter({
-            // Is this allowed? Do I need to create two sep clients or can upstash redis handle this?
-            session: redis,
-            userSession: redis
-        })
+    // TODO: type error here?
+    adapter: kysely(db),
+    // adapter: {
+    //     user: kysely(db),
+    //     session: redisSessionAdapter({
+    //         // Is this allowed? Do I need to create two sep clients or can upstash redis handle this?
+    //         session: redis,
+    //         userSession: redis
+    //     })
+    // },
+    env: dev ? 'DEV' : 'PROD',
+    transformUserData: (userData) => {
+        return {
+            email: userData.email,
+            username: userData.username,
+            default_state_id: userData.default_state_id,
+            userId: userData.id,
+        };
     },
-	env: dev ? 'DEV' : 'PROD',
-	transformUserData: (userData) => {
-		return {
-			email: userData.email,
-			username: userData.username,
-			default_state_id: userData.default_state_id,
-			userId: userData.id,
-		};
-	},
 });
 export type Auth = typeof auth;
