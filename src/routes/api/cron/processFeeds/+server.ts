b637f8a48579db5dbf444f442f8d044bd894fd45
Gus <gus.cuddy@gmail.com>
2023-03-26 12:46:41 -0400

    take processfeed off edge

--- src/routes/api/cron/processFeeds/+server.ts
+++ src/routes/api/cron/processFeeds/+server.ts
@@ -1,9 +1,14 @@
 import { redis } from '$lib/redis';
 import { processFeeds } from '$lib/rss/queue';
+import type { Config } from '@sveltejs/adapter-vercel';
 import type { RequestHandler } from './$types';
+
+export const config: Config = {
+    runtime: "nodejs18.x",
+    split: true
+}
 
 export const GET: RequestHandler = async () => {
     await processFeeds(redis);
     return new Response();
-
 };
