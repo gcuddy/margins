b5e44d57570738ac5e2186597e953787a965075f
Gus <gus.cuddy@gmail.com>
2023-03-31 00:08:47 -0400

    was it the utils all along

--- src/lib/backend/utils.ts
+++ src/lib/backend/utils.ts
@@ -1,10 +1,10 @@
 export async function generateKeyFromUrl(url: string, ext?: string, path = "") {
-    const u = new URL(url)
-    u.search = "";
-    const cleanedUrl = u.toString();
-    const hashBuffer = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(cleanedUrl));
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
-    const Key = path + `${hashHex}${ext ? "." + ext : ""}`
-    return Key;
+    // const u = new URL(url)
+    // u.search = "";
+    // const cleanedUrl = u.toString();
+    // const hashBuffer = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(cleanedUrl));
+    // const hashArray = Array.from(new Uint8Array(hashBuffer));
+    // const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+    // const Key = path + `${hashHex}${ext ? "." + ext : ""}`
+    return Math.random().toString();
 }
