54b639483afd19ca90d52f3aa93e1beab7ba9f79
Gus <gus.cuddy@gmail.com>
2023-04-06 13:46:04 -0400

    experimenting with advanced queries

---
+++ src/routes/tests/entry/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { H1 } from "$lib/components/ui/typography";
+	import type { PageData } from "./$types";
+	export let data: PageData;
+</script>
+
+<H1>{data.entry.title}</H1>
+
+Annotations: {data.entry.annotations.length}
+Collections: {data.entry.collections.length}
+Relations: {data.entry.relations.length}
+Bookmark: {JSON.stringify(data.entry.bookmark)}
+
+<div class="prose prose-slate">
+	{@html data.entry.html}
+</div>
