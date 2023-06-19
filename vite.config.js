f3b6ff00ee27c40c4ee1fb5b6d224cade435139f
Gus <gus.cuddy@gmail.com>
2023-06-19 15:19:30 -0400

    fixed up vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit(), SvelteKitPWA()],
-	resolve: {
-		alias: {
-		  ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
-		}
-	  }
 };
 
 export default config;
