408a24ce9c3df304767dac9b8d96a92250bfa8e2
Gus <gus.cuddy@gmail.com>
2023-08-14 20:16:29 -0400

    fix: keeppreviousdata

--- src/routes/tests/(app2)/+layout.ts
+++ src/routes/tests/(app2)/+layout.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import { QueryClient } from '@tanstack/svelte-query';
+import { QueryClient, keepPreviousData } from '@tanstack/svelte-query';
 import { browser } from '$app/environment';
 
 export async function load({ data }) {
@@ -8,7 +8,8 @@ export async function load({ data }) {
 			queries: {
 				enabled: browser,
 				staleTime: 1000 * 60 * 2, // 2 minutes,
-				gcTime: 1000 * 60 * 60 * 24 // 24 hours (for persistence)
+				gcTime: 1000 * 60 * 60 * 24, // 24 hours (for persistence)
+                placeholderData: keepPreviousData // keep previous data while fetching new data (TODO REVIEW if this is what we want )
 			}
 		}
 	});
