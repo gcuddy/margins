671c4e4b0d3c92db2a3f42cc477d7c99a5c1a5a0
Gus <gus.cuddy@gmail.com>
2023-09-10 12:14:32 -0400

    fix: isvalidurl correctly handles origin === 'null'

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -211,7 +211,7 @@ export const isValidUrl = (text: string) => {
 	// console.log({ text })
 	try {
 		const u = new URL(text);
-		return u.origin !== null || u.origin !== 'null';
+		return u.origin !== null && u.origin !== 'null';
 	} catch {
 		return false;
 	}
