ad83a69d92a6fbc43a5c0ce7796322efce09de1f
Gus <gus.cuddy@gmail.com>
2023-10-29 11:41:50 -0400

    massive purge

--- src/lib/components/AddModal.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-	import AddForm from './AddForm.svelte';
-</script>
-
-<div class="rounded-lg py-4 px-2 font-normal">
-	<AddForm modalControls={true} />
-</div>
--- src/lib/components/ui/dropdown-menu/DropdownMenuSeparator.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Separator from "$lib/components/ui/Separator.svelte";
-</script>
-
-<Separator
-	class="-mx-1 my-1 h-px bg-muted"
-	decorative={false}
-	orientation="horizontal"
-/>
