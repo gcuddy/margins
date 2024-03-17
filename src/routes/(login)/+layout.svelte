21dff2329a8c0b7ee774ad8346822e04dcca9330
Gus <gus.cuddy@gmail.com>
2024-03-17 00:27:12 -0400

    new logo

--- src/routes/(login)/+layout.svelte
+++ src/routes/(login)/+layout.svelte
@@ -1,143 +1,29 @@
 <div class="flex min-h-screen flex-col items-center justify-center">
 	<div
-		class="flex animate-fast-fly-in flex-col items-center justify-center space-y-4"
+		class="animate-fast-fly-in flex flex-col items-center justify-center space-y-4"
 	>
 		<div class="flex">
 			<div class="max-w-xs px-2">
 				<svg
-                    class="h-20 w-20"
-					width="266"
-					height="286"
-					viewBox="0 0 266 286"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<g clip-path="url(#clip0_567_310)">
-						<rect
-							x="135.765"
-							y="0.235352"
-							width="192"
-							height="192"
-							rx="16"
-							transform="rotate(45 135.765 0.235352)"
-							fill="#D97706"
-							fill-opacity="0.97"
-						/>
-						<mask
-							id="mask0_567_310"
-							style="mask-type:alpha"
-							maskUnits="userSpaceOnUse"
-							x="6"
-							y="6"
-							width="259"
-							height="260"
-						>
-							<rect
-								x="135.765"
-								y="0.235352"
-								width="192"
-								height="192"
-								rx="16"
-								transform="rotate(45 135.765 0.235352)"
-								fill="#D97706"
-								fill-opacity="0.97"
-							/>
-						</mask>
-						<g mask="url(#mask0_567_310)">
-							<path
-								d="M262.216 11.3137C268.464 5.06532 278.595 5.06532 284.843 11.3137L397.98 124.451C404.228 130.699 404.228 140.83 397.98 147.078L284.843 260.215C278.595 266.464 268.464 266.464 262.216 260.215L149.079 147.078C142.83 140.83 142.83 130.699 149.079 124.451L205.647 67.8822L262.216 11.3137Z"
-								fill="#FBBF24"
-								fill-opacity="0.85"
-							/>
-							<path
-								d="M188.279 8.48528C192.966 3.79899 200.564 3.79899 205.25 8.48528L324.044 127.279C328.73 131.966 328.73 139.563 324.044 144.25L205.25 263.044C200.564 267.73 192.966 267.73 188.279 263.044L69.4854 144.25C64.7991 139.563 64.7991 131.966 69.4854 127.279L128.882 67.8822L188.279 8.48528Z"
-								fill="#FEF3C7"
-								fill-opacity="0.5"
-							/>
-						</g>
-					</g>
-					<defs>
-						<clipPath id="clip0_567_310">
-							<rect width="266" height="286" fill="white" />
-						</clipPath>
-					</defs>
-				</svg>
-				<!-- <svg
 					class="h-20 w-20"
-					width="260"
-					height="260"
-					viewBox="0 0 260 260"
+					viewBox="0 0 500 500"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 				>
-					<g filter="url(#filter0_d_515_360)">
-						<g clip-path="url(#clip0_515_360)">
-							<rect
-								x="10"
-								y="10"
-								width="240"
-								height="240"
-								rx="120"
-								fill="url(#paint0_radial_515_360)"
-							/>
-							<path d="M166.5 13C89 60.5 187 186 166.5 265" stroke="#FFFBEB" stroke-width="9" />
-							<path
-								d="M193.318 13C115.818 60.5 213.819 186 193.318 265"
-								stroke="#FFFBEB"
-								stroke-width="9"
-							/>
-							<path
-								d="M219.318 19C141.818 66.5 239.819 192 219.318 271"
-								stroke="#FFFBEB"
-								stroke-width="9"
-							/>
-						</g>
-					</g>
-					<defs>
-						<filter
-							id="filter0_d_515_360"
-							x="8"
-							y="9"
-							width="244"
-							height="244"
-							filterUnits="userSpaceOnUse"
-							color-interpolation-filters="sRGB"
-						>
-							<feFlood flood-opacity="0" result="BackgroundImageFix" />
-							<feColorMatrix
-								in="SourceAlpha"
-								type="matrix"
-								values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-								result="hardAlpha"
-							/>
-							<feOffset dy="1" />
-							<feGaussianBlur stdDeviation="1" />
-							<feComposite in2="hardAlpha" operator="out" />
-							<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
-							<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_515_360" />
-							<feBlend
-								mode="normal"
-								in="SourceGraphic"
-								in2="effect1_dropShadow_515_360"
-								result="shape"
-							/>
-						</filter>
-						<radialGradient
-							id="paint0_radial_515_360"
-							cx="0"
-							cy="0"
-							r="1"
-							gradientUnits="userSpaceOnUse"
-							gradientTransform="translate(130 130) rotate(90) scale(120)"
-						>
-							<stop stop-color="#FDE68A" />
-							<stop offset="1" stop-color="#FBBF24" />
-						</radialGradient>
-						<clipPath id="clip0_515_360">
-							<rect x="10" y="10" width="240" height="240" rx="120" fill="white" />
-						</clipPath>
-					</defs>
-				</svg> -->
+					<rect x="4" y="4" width="492" height="492" rx="246" fill="black" />
+					<path
+						d="M122 134.5C122 120.141 133.641 108.5 148 108.5V108.5C162.359 108.5 174 120.141 174 134.5V391.5H122V134.5Z"
+						fill="white"
+					/>
+					<path
+						d="M224 134.5C224 120.141 235.641 108.5 250 108.5V108.5C264.359 108.5 276 120.141 276 134.5V300.01C276 305.135 276 307.698 274.987 309.649C274.133 311.293 272.793 312.633 271.149 313.487C269.198 314.5 266.635 314.5 261.51 314.5H238.49C233.365 314.5 230.802 314.5 228.851 313.487C227.207 312.633 225.867 311.293 225.013 309.649C224 307.698 224 305.135 224 300.01V134.5Z"
+						fill="white"
+					/>
+					<path
+						d="M326 134.5C326 120.141 337.641 108.5 352 108.5V108.5C366.359 108.5 378 120.141 378 134.5V391.5H326V134.5Z"
+						fill="white"
+					/>
+				</svg>
 			</div>
 		</div>
 		<slot />
