4eebbd4833dde79d6d0b581bf81a9b562b637e16
Gus <gus.cuddy@gmail.com>
2024-03-21 13:01:48 -0400

    chore: some updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
 
