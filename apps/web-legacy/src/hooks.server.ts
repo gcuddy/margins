d93bd32b6eea0a0d76cec51c3a1371d53b12a017
Gus <gus.cuddy@gmail.com>
2024-03-27 21:37:34 -0400

    remove old stuff

--- apps/web-legacy/src/hooks.server.ts
+++ apps/web-legacy/src/hooks.server.ts
@@ -1,38 +1,11 @@
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-import { darkThemes } from '$lib/features/settings/themes';
 import { auth } from '$lib/server/lucia';
-
-const handleTheme = (async ({ event, resolve }) => {
-	let theme: string | null = null;
-
-	const newTheme = event.url.searchParams.get('theme');
-	const cookieTheme = event.cookies.get('theme');
-	// TODO: when a theme is selected, add light/dark class depending on theme
-	if (newTheme) {
-		theme = newTheme;
-	} else if (cookieTheme) {
-		theme = cookieTheme;
-	}
-	return await resolve(event, {
-		transformPageChunk: ({ html }) => {
-			console.log({ theme });
-			return html
-				.replace(
-					`data-theme=""`,
-					theme && theme !== 'system' ? `data-theme="${theme}"` : '',
-				)
-				.replace(
-					`class=""`,
-					`class="${theme && darkThemes.includes(theme) ? 'dark' : ''}"`,
-				);
-		},
-	});
-}) satisfies Handle;
 
 const handleAuth = (async ({ event, resolve }) => {
 	const sessionId = event.cookies.get(auth.sessionCookieName);
+	console.log({ sessionId });
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
@@ -59,4 +32,4 @@ const handleAuth = (async ({ event, resolve }) => {
 	return resolve(event);
 }) satisfies Handle;
 
-export const handle: Handle = sequence(handleAuth, handleTheme);
+export const handle: Handle = sequence(handleAuth);
