3ddf3da3688348b62e64cc782c946a496e8ad16d
Gus <gus@guscuddy.com>
2024-09-08 11:52:33 -0400

    unauthorized improvements

--- packages/api/src/server/router.ts
+++ packages/api/src/server/router.ts
@@ -12,43 +12,24 @@ import { AuthorizationError, LuciaLayer } from "../Auth"
 import { Schema } from "@effect/schema"
 // import { ServerRuntime } from "./main"y
 
-// Middleware constructor that logs the name of the middleware
-const authMiddleWare = (name: string) =>
-  HttpMiddleware.make(app =>
-    Effect.gen(function* () {
-      console.log(name) // Log the middleware name when a route is accessed
-      const { Authorization } = yield* HttpServerRequest.schemaHeaders(
-        Schema.Struct({
-          Authorization: Schema.String,
-        }),
-      )
-      const lucia = yield* LuciaLayer
-      // const authorizationHeader =  req.headers
-      const sessionId = Option.fromNullable(
-        lucia.readBearerToken(Authorization),
-      )
-      if (Option.isNone(sessionId)) {
-        // 401 and authorization error
-        // or session not found?
-        yield* new AuthorizationError("No Session Found")
-      }
-      return yield* app // Continue with the original application flow
-    }).pipe(
-      Effect.catchTag("AuthorizationError", () =>
+// TODO: use HttpApi instead / inside here to define declarative api
+export const router = HttpRouter.empty
+  .pipe(
+    HttpRouter.get("/", HttpServerResponse.text("content 1")),
+    HttpRouter.get("/foo", HttpServerResponse.text("content 2")),
+    HttpRouter.mount("/sync", sync),
+    HttpRouter.post("/rpc", HttpRpcRouterNoStream.toHttpApp(appRouter)),
+    HttpRouter.options("*", HttpServerResponse.empty()),
+    HttpRouter.use(flow(HttpMiddleware.cors(), HttpMiddleware.logger)),
+  )
+  .pipe(
+    Effect.catchTags({
+      AuthorizationError: () =>
         HttpServerResponse.text("Unauthorized", { status: 401 }),
-      ),
+    }),
+    Effect.catchAllCause(cause =>
+      HttpServerResponse.text(cause.toString(), { status: 500 }),
     ),
   )
-
-// TODO: use HttpApi instead / inside here to define declarative api
-export const router = HttpRouter.empty.pipe(
-  HttpRouter.get("/", HttpServerResponse.text("content 1")),
-  HttpRouter.get("/foo", HttpServerResponse.text("content 2")),
-  HttpRouter.mount("/sync", sync),
-  HttpRouter.use(authMiddleWare("auth")),
-  HttpRouter.post("/rpc", HttpRpcRouterNoStream.toHttpApp(appRouter)),
-  HttpRouter.options("*", HttpServerResponse.empty()),
-  HttpRouter.use(flow(HttpMiddleware.cors(), HttpMiddleware.logger)),
-)
 //  TODO: Can I use a runtime to avoid this?
 // .pipe(Effect.tapErrorCause(Effect.logError))
--- packages/api/src/server/sync.ts
+++ packages/api/src/server/sync.ts
@@ -1,14 +1,50 @@
 import {
+  HttpMiddleware,
   HttpRouter,
   HttpServerRequest,
   HttpServerResponse,
 } from "@effect/platform"
-import { Effect, Layer } from "effect"
+import { Effect, Layer, Option } from "effect"
 import { Replicache } from "../Replicache"
 import { Schema } from "@effect/schema"
 import { PushRequest } from "../Domain/Replicache"
 import { CurrentUser } from "../Domain/User"
+import { AuthorizationError, LuciaLayer } from "../Auth"
 // import { Replicache } from "../services/Replicache"
+// Middleware constructor that logs the name of the middleware
+
+const authMiddleWare = (name: string) =>
+  HttpMiddleware.make(app =>
+    Effect.gen(function* () {
+      console.log(name) // Log the middleware name when a route is accessed
+      const { Authorization } = yield* HttpServerRequest.schemaHeaders(
+        Schema.Struct({
+          Authorization: Schema.String,
+        }),
+      ).pipe(
+        Effect.catchTag(
+          "ParseError",
+          () => new AuthorizationError("Incorrect Authorization Header"),
+        ),
+      )
+      const lucia = yield* LuciaLayer
+      // const authorizationHeader =  req.headers
+      const sessionId = Option.fromNullable(
+        lucia.readBearerToken(Authorization),
+      )
+      if (Option.isNone(sessionId)) {
+        // 401 and authorization error
+        // or session not found?
+        return yield* new AuthorizationError("No Session Found")
+      }
+      return yield* app // Continue with the original application flow
+    }).pipe(
+      Effect.tapErrorCause(e => Effect.logError("auth middleware error", e)),
+      Effect.catchTag("AuthorizationError", e =>
+        HttpServerResponse.text("Unauthorized", { status: 401 }),
+      ),
+    ),
+  )
 
 // Maybe should use HttpApi instead?
 export const sync = HttpRouter.empty.pipe(
@@ -33,6 +69,7 @@ export const sync = HttpRouter.empty.pipe(
     ),
   ),
   HttpRouter.post("/pull", HttpServerResponse.text("About birds")),
+  HttpRouter.use(authMiddleWare("auth")),
   // Effect.provide(Replicache.Live),
 )
 // .pipe(Layer.provide(Replicache.Live))
