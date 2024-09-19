df6abc726a16446b00b65a5f60ea522e7a80bbb5
Gus <gus@guscuddy.com>
2024-09-19 01:28:39 -0400

    try using mysql2

--- packages/api/src/Sql.ts
+++ packages/api/src/Sql.ts
@@ -1,12 +1,17 @@
 import { Config, ConfigProvider, Logger } from "effect"
-import * as PlanetscaleClient from "./adapters/planetscale.js"
+import { MysqlClient } from "@effect/sql-mysql2"
+// import * as PlanetscaleClient from "./adapters/planetscale.js"
 
-// TODO: fancy nested config thing
-export const SqlLive = PlanetscaleClient.layer(
-  Config.all({
-    host: Config.string("DATABASE_HOST"),
-    username: Config.string("DATABASE_USERNAME"),
-    password: Config.redacted("DATABASE_PASSWORD"),
-  }),
-  // .pipe(Config.nested("database")),
-)
+// // TODO: fancy nested config thing
+// export const SqlLive = PlanetscaleClient.layer(
+//   Config.all({
+//     host: Config.string("DATABASE_HOST"),
+//     username: Config.string("DATABASE_USERNAME"),
+//     password: Config.redacted("DATABASE_PASSWORD"),
+//   }),
+//   // .pipe(Config.nested("database")),
+// )
+
+export const SqlLive = MysqlClient.layer({
+  url: Config.redacted("DATABASE_URL"),
+})
