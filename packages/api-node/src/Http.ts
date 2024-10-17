c39d95bb2e98f732d4f6ac4ec512b2946ad2b17e
Gus <gus@guscuddy.com>
2024-10-16 23:17:44 -0400

    incorporate rpc with http

--- packages/api-node/src/Http.ts
+++ packages/api-node/src/Http.ts
@@ -11,17 +11,23 @@ import { Api } from "./Api.js"
 import { HttpUsersLive } from "./Users/Http.js"
 import { HttpReplicacheLive } from "./Replicache/Http.js"
 import { HttpEntriesLive } from "./Entries/Http.js"
+import { httpRouter } from "./Rpc/Router.js"
 
 const ApiLive = HttpApiBuilder.api(Api).pipe(
   Layer.provide(HttpUsersLive),
   Layer.provide(HttpReplicacheLive),
   Layer.provide(HttpEntriesLive),
 )
-HttpApiBuilder.httpApp
+
+const RpcRouterLive = HttpApiBuilder.Router.use(
+  router => router.concat(httpRouter)
+)
+
 export const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
   Layer.provide(HttpApiSwagger.layer()),
   Layer.provide(HttpApiBuilder.middlewareCors()),
   Layer.provide(ApiLive),
+  Layer.provide(RpcRouterLive),
   HttpServer.withLogAddress,
   Layer.provide(NodeHttpServer.layer(createServer, { port: 3030 })),
 )
--- packages/api-node/src/Rpc/Router.ts
+++ packages/api-node/src/Rpc/Router.ts
@@ -1,26 +1,31 @@
 import { Rpc, RpcRouter } from "@effect/rpc"
 import { GoogleBooksGet, GoogleBooksSearch } from "./Integrations/GoogleBooks/schema.js"
 import { GoogleBooks } from "./Integrations/GoogleBooks/Service.js"
-import { Effect, Layer } from "effect"
+import { Effect, Layer, ManagedRuntime } from "effect"
 import { HttpRpcRouter } from "@effect/rpc-http"
 import { HttpRouter } from "@effect/platform"
 
 
 export const IntegrationsLayer = Layer.mergeAll(
-    GoogleBooks.Live,
-)
-export const router = RpcRouter.make(
-    Rpc.effect(GoogleBooksGet, ({ id }) => GoogleBooks.get(id)),
-    Rpc.effect(GoogleBooksSearch, ({ query }) => GoogleBooks.search(query)),
+  GoogleBooks.Live,
 )
 
+export const rpcRuntime = ManagedRuntime.make(IntegrationsLayer)
+
+export const router = RpcRouter.make(
+  Rpc.effect(GoogleBooksGet, ({ id }) => GoogleBooks.pipe(
+    Effect.andThen(_ => _.get(id))
+  )),
+  Rpc.effect(GoogleBooksSearch, ({ query }) => GoogleBooks.pipe(
+    Effect.andThen(_ => _.search(query))
+  )),
+  // Rpc.effect(GoogleBooksGet, ({ id }) => GoogleBooks.get(id)),
+  // Rpc.effect(GoogleBooksSearch, ({ query }) => GoogleBooks.search(query)),
+)
 export type Router = typeof router
 
-
-export const httpApp = HttpRpcRouter.toHttpApp(router).pipe(
-    // Layer.provide(Integrations),
-)
-
-const http = HttpRouter.empty.pipe(
-    // HttpRouter.post('/rpc', x)
+export const httpRouter = HttpRouter.empty.pipe(
+  HttpRouter.post('/rpc', HttpRpcRouter.toHttpApp(router).pipe(
+    Effect.provide(rpcRuntime)
+  ))
 )
