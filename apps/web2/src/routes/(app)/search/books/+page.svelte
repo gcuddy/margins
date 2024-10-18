55452c1725a3d2f2a4568ce842e59929f5a3a7ec
Gus <gus@guscuddy.com>
2024-10-17 22:15:06 -0400

    debounce runed

--- apps/web2/src/routes/(app)/search/books/+page.svelte
+++ apps/web2/src/routes/(app)/search/books/+page.svelte
@@ -1,11 +1,21 @@
 <script>
 	import { MagnifyingGlass } from 'svelte-radix';
+	import { Debounced } from 'runed';
+
+	let search = $state('');
+	const debounced = new Debounced(() => search, 500);
 </script>
 
 <div class="px-4 py-2 divide-y">
 	<div class="flex gap-2 items-center py-3">
 		<MagnifyingGlass class="size-5" />
-		<input type="text" class="text-6 bg-transparent outline-none text-gray-12" />
+		<input
+			type="text"
+			bind:value={search}
+			class="text-6 bg-transparent outline-none text-gray-12"
+		/>
 	</div>
-	<div></div>
+	<div>
+		{debounced.current}
+	</div>
 </div>
