e887dabf6459926cbbcea47ee6152b6c0c7d5e64
Gus <gus@guscuddy.com>
2024-10-22 01:48:35 -0400

    layout

---
+++ apps/web2/src/routes/(app)/search/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="flex flex-col">
+    <div class="flex">
+        <a href="/search">your stuff</a>
+        <a href="/search/books">Books</a>
+    </div>
+	{@render children()}
+</div>
