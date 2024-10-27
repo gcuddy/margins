0dd4446bbfca511c905b310d9a711b173f209c3d
Gus <gus@guscuddy.com>
2024-10-27 14:27:16 -0400

    yeah

--- apps/web2/src/routes/(app)/search/+page.svelte
+++ apps/web2/src/routes/(app)/search/+page.svelte
@@ -1,24 +1,13 @@
 <script lang="ts">
-	import { getIdRx, Pool } from '$lib/worker/client';
-	import { useRx, useRxSet, useRxValue } from '../profile/rx.svelte';
+	import { Pool } from '$lib/worker/client';
 	import { Effect } from 'effect';
 	import { Search } from '$lib/worker/schema';
 	import { runtime } from '$lib/runtime';
 	import EntryListItem from '$lib/components/entry-list-item.svelte';
-	// import * as Entries from '../profile/Entries';
 	import { Highlight } from '@orama/highlight';
 
-	let search = $state('');
-	console.log({ getIdRx });
-
-	// // $effect(() => {
-	// //     if (search) {
-	// //         getSearch(search)
-	// //     }
-	// // })
-
-	const getSearch = useRxSet(getIdRx);
-	const { value, set } = useRx(getIdRx);
+	let { data } = $props();
+	// let search = $state('');
 
 	const searchEffect = (q: string) =>
 		Pool.pipe(Effect.flatMap((pool) => pool.executeEffect(new Search({ q }))));
@@ -28,57 +17,11 @@
 		CSSClass: 'entry-list-item-highlight'
 	});
 
-	const promise = $derived(runtime.runPromise(searchEffect(search)));
-	// $inspect(promise);
+	const promise = $derived(runtime.runPromise(searchEffect(data.q)));
 
-	// // const
-	// // const results = useRxSet(getIdRx);
-	// $effect(() => {
-	// 	getSearch(search);
-	// });
-	// // $effect(() => {
-	// // 	set(search);
-	// // });
-	// $inspect(value);
-	// $effect(() => {
-	// 	console.log('SEARCH PAGE');
-	// 	const v = value();
-	// 	console.log({ v });
-	// });
-	// // $effect(() => {
-	// // 	console.log({ results });
-	// // 	console.log({ value });
-	// // 	console.log({ set });
-	// // });
-	// // const entries = useRxValue(Entries.effect);
-	// // console.log({ entries });
-	// // let value: readonly string[] = $state([]);
-	// // const res = useRxValue(getIdRx);
-	// // const results = $derived.by(() => {
-	// // 	getSearch;
-	// // 	res;
-	// // 	console.log("deriving")
-	// // 	if (res._tag === 'Success' && entries._tag === 'Success' && entries.value.ready) {
-	// // 		// lol there's gotta be a better way...
-	// // 		return res.value
-	// // 			.map((id) => entries.value.data.find((entry) => entry.id === id))
-	// // 			.filter(Boolean);
-	// 	}
-	// 	return [];
-	// });
-	// $effect(() => {
-	// 	console.log('search', search);
-	// 	// For some reason, have to do this. should figure it out...
-	// 	const res = useRxValue(getIdRx);
-	// 	if (res._tag === 'Success') {
-	// 		value = res.value;
-	// 	}
-	// 	console.log('inside component', useRxValue(getIdRx));
-	// 	// TODO NEXT: use replicache to get entries
-	// });
 </script>
 
-<input type="text" placeholder="Search" bind:value={search} />
+<!-- <input type="text" placeholder="Search" bind:value={search} /> -->
 
 {#await promise}
 	loading...
@@ -93,16 +36,16 @@
 				href={`/${hit.id}`}
 			>
 				{#snippet titleSnippet()}
-					{@const highlighted = highlighter.highlight(hit.document.title ?? '', search)}
+					{@const highlighted = highlighter.highlight(hit.document.title ?? '', data.q)}
 					{@html highlighted.HTML}
 				{/snippet}
 				{#snippet authorSnippet()}
-					{@const highlighted = highlighter.highlight(hit.document.author ?? '', search)}
+					{@const highlighted = highlighter.highlight(hit.document.author ?? '', data.q)}
 					{@html highlighted.HTML}
 				{/snippet}
 
 				{#snippet bodySnippet()}
-					{@const highlighted = highlighter.highlight(hit.document.text ?? '', search)}
+					{@const highlighted = highlighter.highlight(hit.document.text ?? '', data.q)}
 					{#if highlighted.positions.length}
 						{@html highlighted.trim(100)}
 					{/if}
@@ -113,9 +56,3 @@
 {:catch error}
 	{JSON.stringify(error)}
 {/await}
-
-<!-- {results ? JSON.stringify(results) : 'no results'} -->
-
-<!-- <button onclick={() => getSearch(search)}> search </button>
-
-{JSON.stringify(entries)} -->
